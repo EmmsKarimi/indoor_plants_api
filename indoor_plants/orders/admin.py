e76417d0232509930330740b5466d99dd625dc71
Emma Karimi <emmaannkinyua@gmail.com>
2025-04-02 11:25:27 +0300

    Fixed and registered Order model in admin panel

--- indoor_plants/orders/admin.py
+++ indoor_plants/orders/admin.py
@@ -3,6 +3,6 @@ from .models import Order
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('user', 'plant', 'quantity', 'status', 'created_at')
-    search_fields = ('user__username', 'plant__name')
-    list_filter = ('status', 'user')
+    list_display = ('id', 'user', 'plant', 'quantity', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('user__username', 'plant__name', 'status')
