1691517f716e0cbb18c784cd3fd904ecf2895f3d
Emma Karimi <emmaannkinyua@gmail.com>
2025-04-02 11:47:27 +0300

    Registered User model in admin panel

--- indoor_plants/authentication/admin.py
+++ indoor_plants/authentication/admin.py
@@ -4,14 +4,13 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'username', 'is_active', 'is_staff', 'is_superuser')
-    search_fields = ('email', 'username')
+    list_display = ('username', 'email', 'phone_number', 'is_active', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email', 'phone_number')
     list_filter = ('is_active', 'is_staff', 'is_superuser')
-    ordering = ('email',)
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('username', 'date_of_birth', 'profile_photo')}),
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('phone_number',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important Dates', {'fields': ('last_login', 'date_joined')}),
     )
@@ -19,7 +18,6 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'username', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser')}
+            'fields': ('username', 'email', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser')}
         ),
     )
-
