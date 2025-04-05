b4dbfe1b6d636e3e104c9c84a0db554fa477eebb
Emma Karimi <emmaannkinyua@gmail.com>
2025-04-05 23:21:08 +0300

    Fix ALLOWED_HOSTS for local development and prepare for demo

--- indoor_plants/indoor_plants/settings.py
+++ indoor_plants/indoor_plants/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['Emms.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
